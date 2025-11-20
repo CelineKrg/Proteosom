a97ab95467a0c1b98e69dfac80c10b2dde27f4a4
Celine Krogmann <celine.krogmann@stud.uni-heidelberg.de>
2025-11-20 16:32:57 +0100

    "test update"

--- tests/test_file_handling.py
+++ tests/test_file_handling.py
@@ -1,11 +1,9 @@
+from pathlib import Path
 from proteomics.file_handling import read_fasta
+
 def test_read_fasta():
-    tmp_fasta_path = 'data\\dummy-proteins.fasta'
+    tmp_fasta_path = Path("data") / "dummy-proteins.fasta"
     protein_map = read_fasta(tmp_fasta_path)
-    
-    # Replace the strings with your fasta content
-    # which you expect to be now available as a dictionary
+
     assert protein_map["34G7D2"] == "DWRGDSJKLSDFJ"
     assert protein_map["D49J2K"] == "KFPASOKFVVDSKFSL"
-
-test_read_fasta()
